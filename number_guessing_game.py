d32b16571a2d0a2be1a506cbdbe6ff2281a2e524
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2024-10-11 11:00:44 +0200

    Day 2

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -2,33 +2,52 @@
 import pyautogui
 from random import randint
 
-# Function to valid input
 
 def valid_input(x:str) -> bool:
-    if not x.isdigit():
-        pyautogui.alert("Enter a valid input", "Error")
+    """Check if the user input is an integer or not"""
+    if not x.isdigit() or x is None:
+        pyautogui.alert(f"Enter a valid input", f"Error")
         return False
     return True
 
-# Function to get the interval
-def get_interval():
 
+def get_interval() -> list[int]:
+    """Get the interval to generater an random number between it"""
     while True:
         try:
-            a = pyautogui.promt("Enter the lower bound: ", "")
+            a = pyautogui.promt(f"Enter the lower bound: ", f"Input")
             if valid_input(a):
                 a = int(a)
             else:
                 continue
             
-            b = pyautogui.promt("Enter the upper bound: ", "")
+            b = pyautogui.promt(f"Enter the upper bound: ", f"Input")
             if valid_input(b):
-                b = int(a)
+                b = int(b)
             else:
                 continue
 
             if a >= b:
-                pyautogui.alert("The lower bound if superior to your upper bound", "Error")
+                pyautogui.alert(f"The lower bound is superior or equal to your upper bound", f"Error")
+                continue
+            else:
+                return [a,b]
 
-        except:
-            pass
+        except ValueError as e:
+            pyautogui.alert(f"{e}", f"Error")
+
+
+def guess(a:int,b:int) -> int:
+    """Get the user guesseed number that fit in the interval [a,b]"""
+    while True:
+        try:
+            user_num = pyautogui.propmt(f"The number is between {a} and {b}, guess it:", f"Input")
+            if valid_input(user_num):
+                user_num = int(user_num)
+                return user_num
+        except ValueError as e:
+            pyautogui.alert(f'{e}')
+
+
+if __name__ == "__main__":
+    pass
