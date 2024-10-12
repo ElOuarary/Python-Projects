4ac5db1750dd7bd242b60941aa0f265bf4580b41
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2024-10-12 20:45:39 +0100

    Day 3 Part 2

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -2,11 +2,29 @@
 import pyautogui
 from random import randint
 
+
 def play_game() -> bool:
     """Ask the player to either play the game or not"""
-    responce = pyautogui.confirm(f"Do you want to play?", buttons=["Yes", "No"])
+    responce = pyautogui.confirm(f"Do you want to play?", "Main Menu",buttons=["Yes", "No"])
     return responce == "Yes"
 
+
+def welcoming(a:int, b:int, chance:int) -> None:
+    """Display the welcoming Page"""
+    pyautogui.alert(f'''Welcome to the Number Guessing Game!\nI'm thinking of a number between {a} and {b}.\nYou have {chance} chances to guess the correct number.''')
+    
+
+def select_difficulity() -> int:
+    """Select the difficulity level for the number of chances """
+    responce = pyautogui.confirm(f"Select your diffucilty level:", "Configuration", buttons=["Easy","Medium","Hard"])
+    match responce:
+        case "Easy":
+            return 10
+        case "Medium":
+            return 5
+        case _:
+            return 3
+        
 
 def valid_input(x:str) -> bool:
     """Check if the player input is an integer or not"""
@@ -20,13 +38,13 @@ def get_interval() -> list[int]:
     """Get the interval to generater an random number between it"""
     while True:
         try:
-            a = pyautogui.promt(f"Enter the lower bound: ", f"Input")
+            a = pyautogui.prompt(f"Enter the lower bound: ", f"Input")
             if valid_input(a):
                 a = int(a)
             else:
                 continue
             
-            b = pyautogui.promt(f"Enter the upper bound: ", f"Input")
+            b = pyautogui.prompt(f"Enter the upper bound: ", f"Input")
             if valid_input(b):
                 b = int(b)
             else:
@@ -42,14 +60,15 @@ def get_interval() -> list[int]:
             pyautogui.alert(f"{e}", f"Error")
 
 
-def guess(a:int,b:int) -> int:
+def guess(a:int, b:int, chance:int) -> int:
     """Get the player guessed number that fit in the interval [a,b]"""
     while True:
         try:
-            player_num = pyautogui.propmt(f"The number is between {a} and {b}, guess it:", f"Input")
-            if valid_input(player_num) and a <= player_num <= b:
+            player_num = pyautogui.prompt(f"The number is between {a} and {b}, {chance} chances left, guess it:", f"Input")
+            if valid_input(player_num):
                 player_num = int(player_num)
-                return player_num
+                if a <= player_num <= b:
+                    return player_num
         except ValueError as e:
             pyautogui.alert(f'{e}')
 
@@ -57,10 +76,49 @@ def guess(a:int,b:int) -> int:
 def get_hint(player_num:int, computer_num:int) -> None:
     """Get hints to help the player guess the number"""
     if player_num < computer_num:
-        pyautogui.alert(f"Your number is lower.", "Hint")
+        pyautogui.alert(f"Wrong number. Your number is lower.", "Hint")
     else:
-        pyautogui.alert(f"Your number is higher", "Hint")
-    return
+        pyautogui.alert(f"Wrong number. Your number is higher", "Hint")
+    return None
+
+
+def is_guessed(player_num:int, computer_num:int, chance:int) -> bool:
+    if computer_num == player_num:
+        pyautogui.alert(f"Congratulations, you guessed it. It took you {chance} chance", "Result")
+        return True
+    else:
+        get_hint(player_num, computer_num)
+        return False
+
+
+def play() -> None:
+    """Play the game"""
+    # Select the interval and generate the number to guess
+    a, b = get_interval()
+    computer_num = randint(a, b+1)
+
+    # Select the diffucilty and display a welcomin window
+    chance = select_difficulity()
+    welcoming(a, b, chance)
+    
+    # Try to guess the number
+    for attempt in range(1, chance+1):
+        player_num = guess(a, b, chance-attempt+1)
+        if is_guessed(player_num, computer_num, attempt):
+            return None
+    
+    # Displayt the message that the user lost
+    pyautogui.alert(f"You run out of chances, the number to guess was {computer_num}")
+    return None
+    
+
+def main():
+    while True:
+        if play_game():
+            play()
+        else:
+            return None
+
 
 if __name__ == "__main__":
-    pass
+    main()
